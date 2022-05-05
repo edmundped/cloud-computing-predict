a53ddf6c33b0f235d0d7027857d3863352889677
Edmund Dotsey <edmundped@gmail.com>
2022-05-05 19:16:34 +0000

    updated API type

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -26,7 +26,7 @@ $(function () {
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
                 url: "{https://nkbjpuekq8.execute-api.eu-west-1.amazonaws.com/Stage/predict-function}",
                 // ------------------------------------------------------  
-                type: "PUT",
+                type: "PUSH",
                 data: JSON.stringify(json),
                 cache: false,
                 success: function () {
