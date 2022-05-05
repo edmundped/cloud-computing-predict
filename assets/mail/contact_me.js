20af1d02fe9976c4ce73320a7c690e628d85df3a
Edmund Dotsey <edmundped@gmail.com>
2022-05-05 15:14:30 +0000

    edited API url in contact-me.js file

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://nkbjpuekq8.execute-api.eu-west-1.amazonaws.com/Prod}",
+                url: "{https://v00izifxg7.execute-api.eu-west-1.amazonaws.com/api-connection/predict-function}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
